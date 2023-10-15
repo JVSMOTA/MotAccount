fd68cdb5ad477dd3fcb4d5c92afa1ac8d07c7319
JVSMOTA <joao.mota@ccc.ufcg.edu.br>
2023-10-15 14:10:20 -0300

    feat: criando sistemas de rotas e adição de Logo

---
+++ src/pages/SingIn/index.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SingIn() {
+  return (
+    <View>
+      <Text>Tela Login</Text>
+    </View>
+  );
+}
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import SingIn from '../pages/SingIn'
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+                name="SingIn"
+                component={SingIn}
+                options={{ headerShown: false }}
+            />
+            {/* Acima, é como criamos as várias telas. */}
+        </Stack.Navigator>
+    )
+}
