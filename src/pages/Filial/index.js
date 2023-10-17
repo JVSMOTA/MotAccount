5aa9a8adcfefbfcbeba0f08d22d28ba656a6408a
JVSMOTA <joao.mota@ccc.ufcg.edu.br>
2023-10-16 21:38:33 -0300

    feat: criação de tela para Filial

--- src/pages/Filial/index.js
+++ src/pages/Filial/index.js
@@ -1,10 +1,46 @@
 import styles from './styles';
 import { View, Text } from "react-native";
+import InputAmount from '../../components/InputAmount';
+import Button from '../../components/Button';
+import ButtonLogOut from '../../components/ButtonLogOut';
+import ButtonAmmount from '../../components/ButtonAmmount';
+import ButtonSchedules from '../../components/ButtonSchedules';
+import ButtonExpenses from '../../components/ButtonExpenses';
+
+import { useNavigation } from '@react-navigation/native';
+import * as Animatable from 'react-native-animatable';
+import DateTime from '../../components/DateTime';
 
 export default function Filial() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.Container}>
-      <Text>Tela de menu Filial</Text>
+      <View style={styles.HeaderLaranja}>
+      </View>
+      <View style={styles.PageContent}>
+        <Animatable.View
+          animation='fadeInDown'
+          style={styles.MenuTop}>
+          <Text style={styles.Title}>Sapataria Filial</Text>
+          <DateTime/>
+          <InputAmount/>
+          <Button placeholder='Alterar'></Button>
+        </Animatable.View>
+        <Animatable.View
+          style={styles.Menu}>
+          <ButtonAmmount
+            onPress={() => navigation.navigate('ApuradosFilial')}
+          />
+          <ButtonSchedules
+            onPress={() => navigation.navigate('AgendamentosFilial')}
+          />
+          <ButtonExpenses
+            onPress={() => navigation.navigate('DespesasFilial')}
+          />
+        </Animatable.View>
+        <ButtonLogOut placeholder='Sair'></ButtonLogOut>
+      </View>
     </View>
   );
 }
