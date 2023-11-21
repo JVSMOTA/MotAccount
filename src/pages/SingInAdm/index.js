ad5c90ef5f896eb69793d84725ade3dc3c3e9c20
JVSMOTA <joao.mota@ccc.ufcg.edu.br>
2023-11-21 20:29:03 -0300

    🚸 feat: melhorando experiência para ADM

--- src/pages/SingInAdm/index.js
+++ src/pages/SingInAdm/index.js
@@ -1,16 +1,20 @@
-import { LinearGradient } from "expo-linear-gradient";
-import Logo from '../../components/Logo';
-import LoginFormAdm from '../../components/LoginFormAdm';
-import styles from './styles';
+import { LinearGradient } from "expo-linear-gradient"
+import Logo from '../../components/Logo'
+import LoginFormAdm from '../../components/LoginFormAdm'
+import styles from './styles'
+import { Keyboard, Pressable } from "react-native"
 
 export default function SingIn() {
   return (
-    <LinearGradient
-      style={styles.Container}
-      colors={['#FF9B3F', '#DB5605']}
-    >
-      <Logo/>
-      <LoginFormAdm/>
+    <LinearGradient style={styles.Container} colors={['#FF9B3F', '#DB5605']}>
+
+      <Pressable onPress={Keyboard.dismiss}>
+      
+        <Logo/>
+        <LoginFormAdm/>
+      
+      </Pressable>
+    
     </LinearGradient>
-  );
+  )
 }
