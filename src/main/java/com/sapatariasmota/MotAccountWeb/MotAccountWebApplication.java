6ad870316241da848aa92415e8ba361f232dc63f
JVSMOTA <jvmota.sb@gmail.com>
2023-12-17 12:16:41 -0300

    :sparkles: feat: projeto gerado pelo Spring Initializr

---
+++ src/main/java/com/sapatariasmota/MotAccountWeb/MotAccountWebApplication.java
@@ -0,0 +1,13 @@
+package com.sapatariasmota.MotAccountWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotAccountWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotAccountWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapatariasmota/MotAccountWeb/MotAccountWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapatariasmota.MotAccountWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotAccountWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
