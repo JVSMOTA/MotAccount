42b81bd4e02b36ff4201076e3e804db0a760b52f
JVSMOTA <jvmota.sb@gmail.com>
2023-12-18 14:48:57 -0300

    :sparkles: feat: criação de UsuarioRepository.java

--- src/main/java/com/sapatariasmota/MotAccountWeb/repositories/UsuarioRepository.java
+++ src/main/java/com/sapatariasmota/MotAccountWeb/repositories/UsuarioRepository.java
@@ -1,11 +1,12 @@
 package com.sapatariasmota.MotAccountWeb.repositories;
 
 import com.sapatariasmota.MotAccountWeb.models.LojaModel;
+import com.sapatariasmota.MotAccountWeb.models.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<LojaModel, UUID> {
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
 }
