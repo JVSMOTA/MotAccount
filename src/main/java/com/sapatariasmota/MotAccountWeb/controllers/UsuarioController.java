6aaf3fd8fb9a12f9603469db10c82ef4897d6451
JVSMOTA <jvmota.sb@gmail.com>
2023-12-18 14:49:32 -0300

    :sparkles: feat: criação de endpoint POST em UsuarioController.java

--- src/main/java/com/sapatariasmota/MotAccountWeb/controllers/UsuarioController.java
+++ src/main/java/com/sapatariasmota/MotAccountWeb/controllers/UsuarioController.java
@@ -1,4 +1,27 @@
 package com.sapatariasmota.MotAccountWeb.controllers;
 
+import com.sapatariasmota.MotAccountWeb.dtos.UsuarioRecordDto;
+import com.sapatariasmota.MotAccountWeb.models.UsuarioModel;
+import com.sapatariasmota.MotAccountWeb.repositories.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @PostMapping("/usuarios")
+    public ResponseEntity<UsuarioModel> saveUsuario(@RequestBody @Valid UsuarioRecordDto usuarioRecordDto) {
+        var usuarioModel = new UsuarioModel();
+        BeanUtils.copyProperties(usuarioRecordDto, usuarioModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuarioModel));
+    }
 }
