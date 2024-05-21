af03ad35a2dbbb1604d41f3234b4265d84c2b960
JVSMOTA <jvmota.sb@gmail.com>
2024-05-21 15:39:39 -0300

    :art: style: adicionando scroll-bar para div de listagem

--- frontend/src/pages/MenuPrincipal/style.tsx
+++ frontend/src/pages/MenuPrincipal/style.tsx
@@ -16,9 +16,24 @@ export const ContainerData = styled.div`
   width: 100%;
   height: 28vh;
   padding: 9px;
+  padding-right: 5px;
   gap: 2px;
   border-radius: 5px;
   border: 1px solid #DB5605;
+  overflow: hidden;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+    background-color: #FFFFFF;
+    border-radius: 100%;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #DB5605;
+    border-radius: 3px;
+  }
+
 `;
 
 export const Div = styled.div`
